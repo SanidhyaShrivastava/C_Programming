73714735340ddf6bfebfce5242a02ef751130856
EC2 Default User <ec2-user@ip-172-31-30-101.ec2.internal>
2021-04-10 14:16:00 +0000

    a

---
+++ AddingTwoNumber.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+	int number1,number2,answer;
+    scanf("%d\n",& number1);
+    scanf("%d",& number2);
+    answer=number1+number2;
+    printf("number1+number2=%d",answer);
+}
