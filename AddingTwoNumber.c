8a6a659fbfb7de029677a8fc4da981aa238a8054
Sanidhya Shrivastava <77496142+SanidhyaShrivastava@users.noreply.github.com>
2021-04-13 16:57:46 +0530

    Update AddingTwoNumber.c

--- AddingTwoNumber.c
+++ AddingTwoNumber.c
@@ -2,8 +2,10 @@
 
 void main()
 {
-	int number1,number2,answer;
-    scanf("%d\n",& number1);
+    int number1,number2,answer;
+	printf("Enter number1: ");
+    scanf("%d",& number1);
+	printf("Enter number2: ");
     scanf("%d",& number2);
     answer=number1+number2;
     printf("number1+number2=%d",answer);
