ab54975ec3c66f8072b373de3a838ae062c98aa9
EC2 Default User <ec2-user@ip-172-31-30-101.ec2.internal>
2021-04-13 07:32:51 +0000

    a

---
+++ AreaOfCircleCalculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#define pi 3.1459
+
+void main()
+{
+
+	
+	puts("-----------------------------");
+	printf("Circumference of Circle:\n");
+	
+	float radius;
+	scanf("%f",& radius);
+	float circumference;
+	circumference=2*pi*radius;
+	printf("%f\n",circumference);
+	
+	
+	puts("---------------------------------------");
+	printf("Area of Circle:\n");
+	
+	float area;
+	area=pi*radius*radius;
+	printf("%f",area);
+	
+	
+	
+}
